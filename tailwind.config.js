d1a4b0847f636006746dc5bf136aa43c4289546c
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-02-13 10:55:27 -0500

    🌱  Add tailwind config setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "hsl(240, 38%, 20%)",
+        grayishBlue: "hsl(240, 18%, 77%)",
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
